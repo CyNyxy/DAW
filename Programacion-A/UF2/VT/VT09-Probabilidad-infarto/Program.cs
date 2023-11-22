db0b0eba5fa8a13e1cd46b3c395b3b20c06d686b
CyNyxy <94080207+CyNyxy@users.noreply.github.com>
2023-11-22 04:42:20 +0100

    Soluciones VT09

--- Programacion-A/UF2/VT/VT09-Probabilidad-infarto/Program.cs
+++ Programacion-A/UF2/VT/VT09-Probabilidad-infarto/Program.cs
@@ -76,3 +76,45 @@ namespace VT09_Probabilidad_infarto
         }
     }
 }
+
+
+//SOLUCION
+
+//using System;
+
+//namespace VT10_20231120
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            string[,] tablaPorcentajes = { { "5%", "15%" }, { "18", "62" } };
+//            string[,] tablaRespuestas = { { "sobrepeso", "" }, { "stress", "" } };
+//            int[] coordenadas = { 9, 9 };
+
+//            // Se leen los datos de entrada y se convierte en una coordenada
+//            for (int i = 0; i < coordenadas.Length; i++)
+//            {
+//                do
+//                {
+//                    Console.Write("¿Tienes " + tablaRespuestas[i, 0] + "? (S/N): ");
+//                    tablaRespuestas[i, 1] = Console.ReadLine();
+
+//                } while (tablaRespuestas[i, 1].Equals("S") == false && tablaRespuestas[i, 1].Equals("N") == false);
+
+//                // Se concierte S/N en coordenadas que referencien a probabilidades
+//                if (tablaRespuestas[1, i].Equals("S") == false)
+//                {
+//                    coordenadas[i] = 0;
+//                }
+//                else
+//                {
+//                    coordenadas[i] = 0;
+//                }
+//            }
+
+//            // Se escribe en salida la resolución
+//            Console.WriteLine("La probabilidad de sufrir un infarto es del " + tablaPorcentajes[coordenadas[0], coordenadas[1]]);
+//        }
+//    }
+//}
--- Programacion-A/UF2/VT/VT09/Program.cs
+++ Programacion-A/UF2/VT/VT09/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace VT09
 {
@@ -10,17 +6,14 @@ namespace VT09
     {
         static void Main(string[] args)
         {
-            int cifra = 0;
             int[] lista = new int[4];
 
             for (int i = 0; i < lista.Length; i++)
             {
                 Console.Write("Introduce la cifra {0}: ", i);
-                cifra = int.Parse(Console.ReadLine());
+                int cifra = int.Parse(Console.ReadLine());
                 lista[i] = cifra;
             }
-
-
 
             Console.WriteLine(Suma(lista));
 
@@ -33,9 +26,8 @@ namespace VT09
             int suma = 0;
             for (int i = 0; i < lista.Length; i++)
             {
-                suma = suma + lista[i];
+                suma += lista[i];
             }
-            
 
             return "El resultado de la suma es " + suma;
         }
@@ -43,16 +35,89 @@ namespace VT09
 }
 
 
-            //Console.Write("Introduce la cifra 1: ");
-            //int cifra01 = int.Parse(Console.ReadLine());
 
-            //Console.Write("Introduce la cifra 2: ");
-            //int cifra02 = int.Parse(Console.ReadLine());
+// SOLUCION 1
 
-            //Console.Write("Introduce la cifra 3: ");
-            //int cifra03 = int.Parse(Console.ReadLine());
+//using System;
 
-            //Console.Write("Introduce la cifra 4: ");
-            //int cifra04 = int.Parse(Console.ReadLine());
+//namespace VT09_20231113
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            int cifra01 = RecogerNumero(1);
+//            int cifra02 = RecogerNumero(2);
+//            int cifra03 = RecogerNumero(3);
+//            int cifra04 = RecogerNumero(4);
 
-            //Console.WriteLine("El resultado de la suma es " + (cifra01 + cifra02 + cifra03 + cifra04));
+//            Console.WriteLine("El resultado de la suma es " + (cifra01 + cifra02 + cifra03 + cifra04));
+//        }
+
+//        public static int RecogerNumero(int numOperador)
+//        {
+//            Console.Write("Introduce la cifra " + numOperador + ": ");
+
+//            return int.Parse(Console.ReadLine());
+//        }
+//    }
+//}
+
+
+// SOLUCION 2
+
+//using System;
+
+//namespace VT09_20231113
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            int[] cifras = new int[4];
+
+//            for (int i = 0; i < cifras.Length; i++)
+//            {
+//                cifras[i] = RecogerNumero(i + 1);
+//            }
+
+//            Console.WriteLine("El resultado de la suma es " + (cifras[0] + cifras[1] + cifras[2] + cifras[3]));
+//        }
+
+//        public static int RecogerNumero(int numOperador)
+//        {
+//            Console.Write("Introduce la cifra " + numOperador + ": ");
+
+//            return int.Parse(Console.ReadLine());
+//        }
+//    }
+//}
+
+
+// ORIGINAL
+
+//using System;
+
+//namespace VT09_20231113
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.Write("Introduce la cifra 1: ");
+//            int cifra01 = int.Parse(Console.ReadLine());
+
+//            Console.Write("Introduce la cifra 2: ");
+//            int cifra02 = int.Parse(Console.ReadLine());
+
+//            Console.Write("Introduce la cifra 3: ");
+//            int cifra03 = int.Parse(Console.ReadLine());
+
+//            Console.Write("Introduce la cifra 4: ");
+//            int cifra04 = int.Parse(Console.ReadLine());
+
+
+//            Console.WriteLine("El resultado de la suma es " + (cifra01 + cifra02 + cifra03 + cifra04));
+//        }
+//    }
+//}
