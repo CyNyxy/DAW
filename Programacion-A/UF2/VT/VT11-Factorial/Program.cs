50a2d1c28242f1009a085ef27cad9e064d99cff0
CyNyxy <94080207+CyNyxy@users.noreply.github.com>
2023-12-01 01:03:15 +0100

    VT11 Factorial

--- Programacion-A/UF2/VT/VT11-Factorial/Program.cs
+++ Programacion-A/UF2/VT/VT11-Factorial/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace VT11_Factorial
 {
@@ -14,11 +10,36 @@ namespace VT11_Factorial
             // Fórmula: n! = n * (n−1)!
             // 5! = 5 * 4 * 3 * 2 * 1 = 120
 
+            // Método no recursivo
+            //Console.Write("Introduce un número: ");
+            //int num = int.Parse(Console.ReadLine());
+            //int n = num;
+
+            //for (int i = 1; i <= num - 1; i++)
+            //{
+            //    n = n * (num - i);
+            //}
+
+            //Console.Write("El factorial de {0} es {1}", num, n);
+
+            Console.Write("Introduce un número: ");
+            int num = int.Parse(Console.ReadLine());
+
+            int factorial = CalcFactorial(num);
+
+            Console.WriteLine("El factorial de {0} es {1}", num, factorial);
         }
 
-        static void calcFactorial(int num)
+        static int CalcFactorial(int x)
         {
-            
+            if (x == 0)
+            { 
+                return 1; 
+            }
+            else
+            {
+                return x * CalcFactorial(x - 1);
+            }
         }
     }
 }
