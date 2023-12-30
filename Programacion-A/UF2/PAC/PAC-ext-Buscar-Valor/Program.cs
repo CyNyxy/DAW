5ab79f437748051046bc49953bae670ac17c2419
CyNyxy <94080207+CyNyxy@users.noreply.github.com>
2023-12-30 02:29:17 +0100

    PAC ext UF2 added

---
+++ Programacion-A/UF2/PAC/PAC-ext-Buscar-Valor/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace PAC_ext_Buscar_Valor
+{
+    internal class Program
+    {
+        static void BuscarValor(int[] arr, int n)
+        {
+            bool found = false;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == n)
+                {
+                    found = true;
+                    break;
+                }
+            }
+            Console.WriteLine(found ? "Encontrado" : "No encontrado");
+        }
+
+        static void Main()
+        {
+            int[] array = { 2, 4, 6, 8, 10 };
+            BuscarValor(array, 7);
+        }
+    }
+}
---
+++ Programacion-A/UF2/PAC/PAC-ext-Triangulo-rectangulo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PAC_ext_Triangulo_rectangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ImprimirPatron(4);
+        }
+
+        static void ImprimirPatron(int n) 
+        { 
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
