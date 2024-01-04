4fcb1f01c0aa648e28c4b356121678e22df01c52
CyNyxy <94080207+CyNyxy@users.noreply.github.com>
2024-01-04 05:58:35 +0100

    Gastos mes y clases UF3 added

---
+++ Programacion-A/UF3/Gastos-mes/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace Gastos_mes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string patron = "Gastos_*_??????.txt";
+
+            string[] paths = Directory.GetFiles(Utils.OP_Ficheros.dir_ficheros, patron);
+
+            Console.Write("|-------------------------------------------------------------|\n");
+            Console.Write("|    FECHA    |    CONCEPTO                   |    IMPORTE    |\n");
+            Console.Write("|-------------------------------------------------------------|\n");
+
+            foreach (string path in paths)
+            {
+                FileStream x = Utils.OP_Ficheros.AbrirFicheroLectura(path);
+                Utils.OP_Ficheros.DameStreamLectura(x);
+            }
+
+            Console.Write("|-------------------------------------------------------------|\n");
+        }
+    }
+}
---
+++ Programacion-A/UF3/Utils/Class1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+
+namespace Utils
+{
+    public class OP_Ficheros
+    {
+        // Path del directorio raíz de trabajo con los ficheros de empresa
+        public const string dir_ficheros = "C:\\Users\\Nyxy\\Desktop\\Ficheros\\";
+
+        public static bool ExisteFichero(string Ruta)
+        /*
+         * Comprueba que existe un determinado fichero
+         *      Retorno: Boolean
+         */
+        {
+            if (File.Exists(Ruta))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static FileStream AbrirFicheroLectura(string Fichero)
+        {
+            FileStream F1 = new FileStream(Fichero, FileMode.Open, FileAccess.Read);
+
+            return F1;
+        }
+
+        //public static FileStream AbrirFicheroEscritura(string Fichero)
+
+        //public static FileStream CrearFicheroEscritura(string Fichero)
+
+        public static StreamReader DameStreamLectura(FileStream Fichero)
+        {
+
+            StreamReader sr = new StreamReader(Fichero);
+            string linea = "";
+
+            while ((linea = sr.ReadLine()) != null)
+            {
+                string fecha = "|   " + linea.Substring(0, 8) + "  | " + linea.Substring(8) + "    |";
+                Console.WriteLine(fecha);
+            }
+            sr.Close();
+            Fichero.Close();
+
+            return sr;
+        }
+
+
+        //public static StreamWriter DameStreamEscritura(FileStream Fichero)
+    }
+}
